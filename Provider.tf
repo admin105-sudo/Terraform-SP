162d805d759f6bc27454fd8a7663fc5449a75ff7
ABI <abiofficial1805@gmail.com>
2025-08-06 11:57:28 +0530

    Create Provider.tf

---
+++ Provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "us-west-1"  # You can change this to your preferred region
+  access_key = "AKIAZCJFZHW66REIVL53"
+  secret_key = "YmVmeg1hdxyRaXOUZz++tSM2L+dPymF0r/c14wti"
+}
